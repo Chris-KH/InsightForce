83512f6b5161906164163987c43ab4b25d47db12
Chris-KH <klowgamervn@gmail.com>
2026-04-01 00:53:04 +0700

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([]);
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
